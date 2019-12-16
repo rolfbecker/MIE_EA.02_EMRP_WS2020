6627b068b4812e3aa71e697ca590d107db91d070
vji-axelor <vji.axelor@gmail.com>
2019-12-16 11:09:21 +0530

    set basic model

---
+++ axelor-rku/src/main/java/com/axelor/apps/rku/web/BranchController.java
@@ -0,0 +1,24 @@
+package com.axelor.apps.rku.web;
+
+import java.util.List;
+
+import com.axelor.apps.rku.db.Branch;
+import com.axelor.apps.rku.db.Semester;
+import com.axelor.apps.rku.db.repo.SemesterRepository;
+import com.axelor.inject.Beans;
+import com.axelor.rpc.ActionRequest;
+import com.axelor.rpc.ActionResponse;
+
+public class BranchController {
+
+  public void selectSemester(ActionRequest request, ActionResponse response) {
+    Branch branch = request.getContext().asType(Branch.class);
+
+    if (branch.getBranchConfig() == null) {
+      return;
+    }
+     String filter = "self.branchConfig.name = '" + branch.getBranchConfig().getName() + "'";
+     List<Semester> semesters = Beans.get(SemesterRepository.class).all().filter(filter).fetch();
+     response.setValue("semesterList", semesters);
+  }
+}
